92ad41bdd50dd5eba22a8166ea58e733c26a5640
Floyd Pink <imbleedingme@googlemail.com>
2012-09-30 21:17:59 -0400

    changes to java files for GCMPlugin
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- src/com/plugin/GCM/GCMPlugin.java
+++ src/com/plugin/GCM/GCMPlugin.java
@@ -1,9 +1,7 @@
 package com.plugin.GCM;
 
-
 //import java.io.*;
 //import java.util.*;
-
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,95 +14,69 @@ import org.apache.cordova.api.PluginResult;
 import org.apache.cordova.api.PluginResult.Status;
 import com.google.android.gcm.*;
 
-
-/**
- * @author awysocki
- *
- */
-
 public class GCMPlugin extends Plugin {
 
-  public static final String ME="GCMPlugin";
+	public static final String ME = "GCMPlugin";
 
-  public static final String REGISTER="register";
-  public static final String UNREGISTER="unregister";
+	public static final String REGISTER = "register";
+	public static final String UNREGISTER = "unregister";
 
-  public static Plugin gwebView;
-  private static String gECB;
-  private static String gSenderID;
+	public static Plugin gwebView;
+	private static String gSenderID;
 
-  @SuppressWarnings("deprecation")
-@Override
-  public PluginResult execute(String action, JSONArray data, String callbackId)
-  {
+	@SuppressWarnings("deprecation")
+	@Override
+	public PluginResult execute(String action, JSONArray data, String callbackId) {
 
-    PluginResult result = null;
+		PluginResult result = null;
 
-    Log.v(ME + ":execute", "action=" + action);
+		Log.v(ME + ":execute", "action=" + action);
 
-    if (REGISTER.equals(action)) {
+		if (REGISTER.equals(action)) {
 
-      Log.v(ME + ":execute", "data=" + data.toString());
+			Log.v(ME + ":execute", "data=" + data.toString());
 
-      try {
+			try {
 
-        JSONObject jo= new JSONObject(data.toString().substring(1, data.toString().length()-1));
+				JSONObject jo = new JSONObject(data.toString().substring(1,
+						data.toString().length() - 1));
 
-        gwebView = this;
+				gwebView = this;
 
-        Log.v(ME + ":execute", "jo=" + jo.toString());
+				Log.v(ME + ":execute", "jo=" + jo.toString());
 
-        gECB = (String)jo.get("ecb");
-        gSenderID = (String)jo.get("senderID");
+				gSenderID = (String) jo.get("senderID");
 
-        Log.v(ME + ":execute", "ECB="+gECB+" senderID="+gSenderID );
+				Log.v(ME + ":execute", "senderID=" + gSenderID);
 
-        GCMRegistrar.register(this.cordova.getContext(), gSenderID);
+				GCMRegistrar.register(this.cordova.getContext(), gSenderID);
 
+				Log.v(ME + ":execute", "GCMRegistrar.register called ");
 
-        Log.v(ME + ":execute", "GCMRegistrar.register called ");
+				result = new PluginResult(Status.OK);
+			} catch (JSONException e) {
+				Log.e(ME, "Got JSON Exception " + e.getMessage());
+				result = new PluginResult(Status.JSON_EXCEPTION);
+			}
+		} else if (UNREGISTER.equals(action)) {
 
-        result = new PluginResult(Status.OK);
-      }
-      catch (JSONException e) {
-        Log.e(ME, "Got JSON Exception "
-          + e.getMessage());
-        result = new PluginResult(Status.JSON_EXCEPTION);
-      }
-    }
-    else if (UNREGISTER.equals(action)) {
+			GCMRegistrar.unregister(this.cordova.getContext());
+			Log.v(ME + ":" + UNREGISTER, "GCMRegistrar.unregister called ");
 
-      GCMRegistrar.unregister(this.cordova.getContext());
-      Log.v(ME + ":" + UNREGISTER, "GCMRegistrar.unregister called ");
+		} else {
+			result = new PluginResult(Status.INVALID_ACTION);
+			Log.e(ME, "Invalid action : " + action);
+		}
 
-    }
-    else
-    {
-      result = new PluginResult(Status.INVALID_ACTION);
-      Log.e(ME, "Invalid action : "+action);
-    }
+		return result;
+	}
 
-    return result;
-  }
-
-
-  public static void sendJavascript( JSONObject _json )
-  {
-    String _d =  "javascript:"+gECB+"(" + _json.toString() + ")";
-        Log.v(ME + ":sendJavascript", _d);
-
-        if (gECB != null ) {
-          gwebView.sendJavascript( _d );
-        }
-  }
-
-
-  /**
-   * Gets the Directory listing for file, in JSON format
-   * @param file The file for which we want to do directory listing
-   * @return JSONObject representation of directory list. e.g {"filename":"/sdcard","isdir":true,"children":[{"filename":"a.txt","isdir":false},{..}]}
-   * @throws JSONException
-   */
-
+	public static void sendJavascript(JSONObject _json,
+			String callbackMethodName) {
+		String _d = "javascript:" + callbackMethodName + "(" + _json.toString()
+				+ ")";
+		Log.v(ME + ":sendJavascript", _d);
+		gwebView.sendJavascript(_d);
+	}
 
 }
